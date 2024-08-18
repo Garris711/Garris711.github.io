d286bd3aa5549f6683fb3c413cf49086b6f5a713
NyanLinHtet <u6430031@au.edu>
2024-08-19 01:54:57 +0700

    Deploy my-react-app

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,20 +1,21 @@
 import * as React from "react";
-import EditContact, {action as editAction,} from "./routes/edit";
-import ErrorPage from "./error-page";
-import Root, { loader as rootLoader,action as rootAction, } from "./routes/root";
 import * as ReactDOM from "react-dom/client";
-import { action as destroyAction } from "./routes/destroy";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
+import ErrorPage from "./error-page";
 import Index from "./routes/index";
 import Contact, {
   loader as contactLoader,
   action as contactAction,
 } from "./routes/contact";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import EditContact, { action as editAction } from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
 import "./index.css";
 
+// Define your routes
 const router = createBrowserRouter([
   {
     path: "/",
@@ -42,10 +43,10 @@ const router = createBrowserRouter([
         errorElement: <div>Oops! There was an error.</div>,
       },
     ],
-
   },
 ]);
 
+// Render the app and attach it to the root element
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} basename="/my-react-app" />
